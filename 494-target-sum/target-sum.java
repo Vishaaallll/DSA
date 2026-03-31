f097deff9b2116fc054e483c80e9ee149634f333
Vishaaallll <vy902660@gmail.com>
2026-03-31 22:24:52 +0530

    Time: 8 ms (60.71%) | Memory: 46.7 MB (28.92%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,20 @@
 class Solution {
-    public int NumberOfWays(int i, int target , int [] nums){
+    int sum = 0;
+    public int NumberOfWays(int i, int target ,int res , int [] nums , int [][] dp){
         if(i == nums.length){
-            if(target == 0) return 1;
+            if(res == target) return 1;
             else return 0;
         }
-        int add = NumberOfWays(i+1, target-nums[i], nums);
-        int sub = NumberOfWays(i+1, target + nums[i], nums);
-        return add+ sub;
+        if(dp[i][res+sum] != -1) return dp[i][res+sum];
+        int add = NumberOfWays(i+1, target, res - nums[i], nums,dp);
+        int sub = NumberOfWays(i+1, target, res + nums[i], nums,dp);
+        return dp[i][res +sum] = add + sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return NumberOfWays(0,target, nums);
+        sum = 0;
+        for(int num : nums) sum += num;
+        int [][] dp = new int [nums.length][2*sum+1];
+        for(int [] arr : dp) Arrays.fill(arr,-1);
+        return NumberOfWays(0,target,0, nums ,dp);
     }
 }
