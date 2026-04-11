be32fa370fe0979650bc313fb2b326ff379f7493
Vishaaallll <vy902660@gmail.com>
2026-04-11 23:09:00 +0530

    Time: 1 ms (67.65%) | Memory: 43.7 MB (22.65%) - LeetSync

--- 4275-traffic-signal-color/traffic-signal-color.java
+++ 4275-traffic-signal-color/traffic-signal-color.java
@@ -1,10 +1,17 @@
 class Solution {
     public String trafficSignal(int timer) {
-        return switch (timer) {
-    case 0 -> "Green";
-    case 30 -> "Orange";
-    case Integer t when t > 30 && t <= 90 -> "Red";
-    default -> "Invalid";
-};
+       if(timer==0){
+ return "Green"; }
+
+ else if(timer==30){
+   return "Orange";}
+
+ else if(timer>30   && timer<=90){
+ return "Red"; }
+
+ else{
+   return "Invalid"; }
+
+
     }
 }
