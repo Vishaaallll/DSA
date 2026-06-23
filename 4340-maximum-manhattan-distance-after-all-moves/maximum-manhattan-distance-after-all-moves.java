b7d3946279a9a6d81984622a92a0585d13cbb3df
Vishaaallll <vy902660@gmail.com>
2026-06-24 00:13:32 +0530

    Time: 17 ms (58.02%) | Memory: 47.5 MB (99.51%) - LeetSync

---
+++ 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDistance(String moves) {
+        int x = 0;
+        int y =0;
+        int underscore = 0;
+        for(int i =0; i < moves.length(); i++){
+            char c = moves.charAt(i);
+            if( c == 'L') x += -1;
+            else if(c == 'R') x += 1;
+            else if(c == 'U') y += 1;
+            else if(c == 'D') y += -1;
+            else underscore++;
+        }
+        return Math.abs(0-x) + Math.abs(0 - y) + underscore;
+    }
+}
