807d6787daa38584dd39c38e4095a50e105d93a1
Vishaaallll <vy902660@gmail.com>
2026-06-28 02:44:09 +0530

    Time: 122 ms (25.30%) | Memory: 106 MB (5.53%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,28 +1,45 @@
 class Solution {
-    public boolean isPalindrome(String s){
-        int i =0;
-        int j = s.length()-1;
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
     public String longestPalindrome(String s) {
         int n = s.length();
-        String ans = s.substring(0, 1);
-        for(int i =0; i<n; i++){
-            for(int j= i+1; j <= n;j++){
-                String res = s.substring(i,j);
-                if(isPalindrome(res)){
-                    if(res.length() > ans.length())
-                     ans = res;
+        int max = Integer.MIN_VALUE;
+        int dx = -1 , dy = -1;
+        int [][] dp = new int[n][n];
+        
+        for(int k =0; k < n; k++){
+            int i = 0 , j = k;
+            while(j < n){
+                if(i == j) {
+                    dp[i][j] = 1;
+                    if(max < j - i +1){
+                        max = j -i +1;
+                        dx = i;
+                        dy = j;
+                    }
                 }
+                 else if(i == j-1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = 1;
+                        if(max < j - i +1){
+                        max = j -i +1;
+                        dx = i;
+                        dy = j;
+                    }
+                    }
+                }   
+                else if(s.charAt(i) == s.charAt(j)){
+                    if(dp[i+1][j-1] == 1){
+                        dp[i][j] = 1;
+                        if(max < j - i +1){
+                        max = j -i +1;
+                        dx = i;
+                        dy = j;
+                    }
+                    }
+                }
+                
+                i++; j++;
             }
         }
-        return ans;
+        return s.substring(dx,dy+1);
     }
 }
